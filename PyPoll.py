8758431802dfa5a0e2fe4fc821abf491f5e32b96
Kaleb Carter <kcart98@att.net>
2021-06-22 23:40:09 -0600

    Added reader function

--- PyPoll.py
+++ PyPoll.py
@@ -13,8 +13,11 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 # Open the election results and read the file
 with open(file_to_load) as election_data:
 
-     # To do: perform analysis.
-     print(election_data)
+     # To do: read and analyze the data here.
+     file_reader = csv.reader(election_data)
+     
+     headers = next(file_reader)
+     print(headers)
 
 # Create a filename variable to a direct or indirect path to the file.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
